928e071d26bc8bc87192b746e4608b8f1c49960e
coderkarma <karmadrukpa11@gmail.com>
2020-03-19 17:41:43 -0700

    simple todo with add, delete functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name: string = "Karma";
+  name: string = "Todo App";
 }
--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
 import { Todo } from "src/app/models/Todo";
+import { TodoService } from "../../services/todo.service";
 
 @Component({
   selector: "app-todo-item",
@@ -8,7 +9,9 @@ import { Todo } from "src/app/models/Todo";
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
-  constructor() {}
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {}
 
@@ -20,5 +23,17 @@ export class TodoItemComponent implements OnInit {
       "is-complete": this.todo.completed
     };
     return classes;
+  }
+
+  onToggle(todo) {
+    // toggle in UI
+    todo.completed = !todo.completed;
+
+    // TOGGLE IN SERVER
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
   }
 }
